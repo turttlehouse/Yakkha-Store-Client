c94ac5590d6a5195942d5282fdfef370f35339c4
turttlehouse <builandrun464@gmail.com>
2025-01-20 22:54:05 +0545

    Basic Folder setup : register,login,home page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from "./store/store";
+import Register from "./pages/auth/register/Register";
+import Login from "./pages/auth/login/Login";
+import Home from "./pages/home/Home";
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/register" element={<h1>Register Page</h1>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path = "/login" element = {<Login/>}/>
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </BrowserRouter>
