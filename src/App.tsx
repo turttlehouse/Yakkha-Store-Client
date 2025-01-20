85e9d9391fea394867c5611fe8394735b952f581
turttlehouse <builandrun464@gmail.com>
2025-01-20 08:20:52 +0545

    routing setup and connecting react and redux toolkit store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from "./store/store";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/register" element={<h1>Register Page</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
