471fa7109e4fd57aad572152fdf88178c8179158
turttlehouse <builandrun464@gmail.com>
2025-01-22 07:43:54 +0545

    Infering types for dispatch and selector in store  and hooks with types made in hooks file

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,3 +9,9 @@ const store = configureStore({
 })
 
 export default store
+
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+//typeof store.dispatch is used to infer the type of store.dispatch. This means that AppDispatch will have the same type as store.dispatch without you needing to manually specify what that type is. This is useful because it ensures that AppDispatch will always be in sync with the actual type of store.dispatch, even if it changes in the future.
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
